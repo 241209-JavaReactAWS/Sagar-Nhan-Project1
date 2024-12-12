d34059ddac8415a2130da6856fc834d2af376b19
Nhan Dang <nhand.tech@gmail.com>
2024-12-12 17:46:55 -0600

    working on home page

--- design/events.js
+++ design/events.js
@@ -1,6 +1,8 @@
 let usernameInput = document.getElementById("usernameInput")
 let passwordInput = document.getElementById("passwordInput")
 let registerButton = document.getElementById ("createUserButton")
+let navbutton = document.querySelectorAll('.nav-bar')
+let sellectButton = document.querySelectorAll('.button')
 
 // Create a function that we expect to be called when the button is clicked
 function submitNewUser(){
@@ -20,5 +22,32 @@ function submitNewUser(){
     console.log(user)
 
 }
+
+function removeActive(){
+    sellectButton.forEach(sellectButton => sellectButton.classList.remove('active'));
+}
+sellectButton.forEach(button => {
+    button.addEventListener('click', () => {
+        removeActive();
+        button.classList.add('active');
+
+        let buttonId = button.id;
+         switch (buttonId) {
+            case 'home':
+                window.location.href = 'home.html'; // Navigate to home page
+                break;
+            case 'contact':
+                window.location.href = 'contact.html'; // Navigate to contact page
+                break;
+            case 'account':
+                window.location.href = 'login.html'; // Navigate to account page
+                break;
+            case 'cart':
+                window.location.href = 'cart.html'; // Navigate to cart page
+         }
+    })
+})
+
+
 registerButton.addEventListener('click', submitNewUser)
 
