30c762d8917e2fd064a273b8c72f904b066c64cc
Sagar Patel <sagar.patel19nov@gmail.com>
2024-12-16 14:58:10 -0500

    Carousel added

--- design/events.js
+++ design/events.js
@@ -9,21 +9,35 @@ let dropdowntable = document.querySelector("#account .dropdown");
 let products = [
     {
         id: 1,
-        title: "The Great Gatsby",
-        author: "F. Scott Fitzgerald",
-        genre: "Classic",
+        name: "1971 Pontiac GTO Judge",
         price: 10.99,
+        quantity: 5,
         rating: 4.8,
-        imageUrl: "./assets/TheGreatGatsby.jpg"
+        imageUrl: "../assets/car1"
     },
     {
         id: 2,
-        title: "To Kill a Mockingbird",
-        author: "Harper Lee",
-        genre: "Fiction",
-        price: 12.99,
-        rating: 4.9,
-        imageUrl: "../assets/book-cover-To-Kill-a-Mockingbird-many-1961.webp"
+        name: "The Monkeemobile",
+        price: 510.99,
+        quantity: 5,
+        rating: 4.8,
+        imageUrl: "../assets/car2"
+    },
+    {
+        id: 3,
+        name: "Car",
+        price: 101.99,
+        quantity: 5,
+        rating: 4.8,
+        imageUrl: "../assets/car3"
+    },
+    {
+        id: 4,
+        name: "1971 Pontiac GTO Judge",
+        price: 120.99,
+        quantity: 5,
+        rating: 4.8,
+        imageUrl: "../assets/car4"
     },
     // Add more predefined products as needed...
 ];
@@ -32,10 +46,10 @@ let products = [
 function createProductCard(product) {
     return `
         <div class="card" style="width: 18rem; flex: 0 0 auto;">
-            <img src="${product.imageUrl}" class="card-img-top" alt="${product.title}">
+            <img src="${product.imageUrl}" class="card-img-top" alt="${product.name}">
             <div class="card-body">
-                <h5 class="card-title">${product.title}</h5>
-                <p class="card-text">By: ${product.author}</p>
+                <h5 class="card-title">${product.name}</h5>
+                <p class="card-text">By: ${product.quantity}</p>
                 <p class="card-text text-primary fw-bold">$${product.price.toFixed(2)}</p>
                 <p class="card-text text-warning">Rating: ⭐${product.rating}</p>
                 <a href="#" class="btn btn-primary">Add to Cart</a>
@@ -44,6 +58,11 @@ function createProductCard(product) {
     `;
 }
 
+//Slide the images on the Home page 
+function slideImage() {
+    let slidingImage = document.getElementById("slideImage");
+    
+}
 // Function to render the current list of products in the "New Arrivals" section
 function renderProducts() {
     let productsContainer = document.getElementById('products');
